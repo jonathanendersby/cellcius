1ce51ac83920089ccfbc2d18f7497bc223a065dd
Jonathan Endersby <jonathan@endersby.co.za>
2013-03-02 08:19:02 +0200

    Cleaning up docstring

--- cellcius.py
+++ cellcius.py
@@ -1,23 +1,13 @@
 """CellCius is an automatic balance checking tool for the Cell-C mobile network
 in South Africa. 
 
-View settings.py for more info on configuring
+View README.md and settings.py(.sample) for more info on configuration.
 
-Why did I write this? 
-Because it annoys me that an MNO doesn't send warnings for this kind thing. 
-Not receiving warnings means that once your data bundle is up, you'll 
-chew through your remaining "airtime" at 99c per mb instead of the 
-in-bundle 15c.
-
-It's also very annoying running out of airtime during an important call.
-
-You'll need to set this up on with cron (or whatever).
 """
 
 import requests
 import re
 from lxml.html import fromstring 
-
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
