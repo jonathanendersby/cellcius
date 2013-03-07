410f5e2cdebe90a7be3b4930d1972811a9314232
Jonathan Endersby <jonathan@endersby.co.za>
2013-03-07 17:04:10 +0200

    Fixing Cell-C oddity where user can have 0 minutes, but still have a rand balance, and can therefore still make calls.
    I'm not sure how it happened, but it seems like depending on which
    system you use to recharge, there is a scenario where you can have
    0 minutes but have a rand balance that still allows you to make
    calls.
    
    I found the page that returns this rand value and do a simple
    calculation to determine the real number of minutes available.
    
    The settings file now has a variable called cellc_voice_per_minute
    which is currently set to 0.99.

--- cellcius.py
+++ cellcius.py
@@ -21,6 +21,7 @@ sso_url = 'https://sso.cellc.co.za/ca/loginbox.do?service=https%3A%2F%2Fecare.ce
 post_url = 'https://sso.cellc.co.za/ca/POST.do'
 post_url2 = 'https://ecare.cellc.co.za/ecare/post.do'
 stats_url = 'https://ecare.cellc.co.za/ecare/common/landingPage.do?method=getAjaxUsageTraffic'
+rand_balance_url = 'https://ecare.cellc.co.za/ecare/mycellc/mobile/invoice/balance/'
 
 s = requests.session()
 r = s.get(sso_url)
@@ -57,9 +58,16 @@ x = s.get(stats_url)
 
 summary = " ".join(x.text.split())
 
+# Query the actual balance from a different page because cell-c.
+r = s.get(rand_balance_url)
+summary2 = " ".join(r.text.split())
+
+
 data = re.match(".*Inclusive data remaining: (.*?)&nbsp;.*", summary)
 sms = re.match(".*Inclusive SMS's remaining: (.*?)&nbsp;.*", summary)
 voice = re.match(".*voice minutes remaining:&nbsp; (.*?)&nbsp;.*", summary)
+voice = re.match(".*voice minutes remaining:&nbsp; (.*?)&nbsp;.*", summary)
+rand_balance = re.match('''.*\<th\>Balance\:\<\/th\> \<td\>R(.*?)\<\/td\>.*''', summary2)
 
 if data:
     data = float(data.groups()[0])
@@ -69,17 +77,28 @@ if sms:
     
 if voice:
     voice = float(voice.groups()[0])
+else:
+    voice = 0
+    
+if rand_balance:
+    rand_balance = float(rand_balance.groups()[0])
+else:
+    rand_balance = 0
+
+combined_voice = int(voice + (rand_balance/settings.cellc_voice_per_minute))
 
 if not settings.quiet:
     print "Data: " + str(data) + ' mb'
     print "SMS: " + str(sms)
     print "Voice: " + str(voice) + ' minutes'
+    print "Balance: R" + str(rand_balance)
+    print "Combined Voice: " + str(combined_voice) + " minutes"
 
 ### SENDING EMAIL
 
 if ((settings.threshold_sms and sms < settings.threshold_sms) or \
     (settings.threshold_data and data < settings.threshold_data) or \
-    (settings.threshold_voice and voice < settings.threshold_voice)):
+    (settings.threshold_voice and combined_voice < settings.threshold_voice)):
     if not settings.quiet:
         print "Under Threshold"
     
@@ -104,7 +123,7 @@ if ((settings.threshold_sms and sms < settings.threshold_sms) or \
         msg['To'] = settings.send_to
         msg['Subject'] = 'Cell-C balance warning for ' + settings.msisdn
 
-        body = ("Voice: %s Minutes (%s)\nData: %s MB (%s)\nSMSes: %s (%s)\n\n\nFor MSISDN: %s\n(Threshold shown in brackets)" % (voice, settings.threshold_voice, data, settings.threshold_data, sms, settings.threshold_sms, settings.msisdn))
+        body = ("Voice: %s Minutes (%s)\nData: %s MB (%s)\nSMSes: %s (%s)\n\n\nFor MSISDN: %s\nThreshold shown in brackets. Voice is a combined total of %s minutes and R%s." % (combined_voice, settings.threshold_voice, data, settings.threshold_data, sms, settings.threshold_sms, settings.msisdn, voice, rand_balance))
 
         msg.attach(MIMEText(body))
 
